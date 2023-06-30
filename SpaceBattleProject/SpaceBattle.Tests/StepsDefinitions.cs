3b53ddec3f9b870f9d60854970b32229190e680a
IvanyutinSA <ivanyutin2004@gmail.com>
2023-06-30 12:39:59 +0000

    Some changes in Spaceship class structure

--- SpaceBattleProject/SpaceBattle.Tests/StepsDefinitions.cs
+++ SpaceBattleProject/SpaceBattle.Tests/StepsDefinitions.cs
@@ -2,20 +2,28 @@ namespace SpaceBattle.Tests;
 using SpaceBattleLib;
 using TechTalk.SpecFlow;
 
-[Binding]
-public class SpaceBattle_Tests
+[Binding, Scope(Feature =  "Расход топлива")]
+public class SpaceshipFuel
 {
-    private SpaceBattle _spaceShip = new SpaceBattle();
+    private Spaceship _spaceShip = new Spaceship();
+    private Exception _actualException = new Exception();
+}
+
+
+[Binding, Scope(Feature = "Равномерное движение корабля")]
+public class SpaceshipUniformMotion_Tests
+{
+    private Spaceship _spaceShip = new Spaceship();
     private Exception _actualException = new Exception();
     [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
     public void CantDefinePosition()
     {
-        _spaceShip.SetCurrentPosition(new double[] {double.NaN, double.NaN});
+        _spaceShip.Position.Position = new double[] {double.NaN, double.NaN};
     }
     [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
     public void HasInstantSpeed(double x, double y)
     {
-        _spaceShip.SetInstantSpeed(new double[] {x, y});
+        _spaceShip.Engine.UniformMotionSpeed = new double[] {x, y};
     }
     
     [When(@"происходит прямолинейное равномерное движение без деформации")]
@@ -23,7 +31,7 @@ public class SpaceBattle_Tests
     {
         try
         {
-            _spaceShip.UniformMovementStep();
+            _spaceShip.UniformMotion();
         }
         catch(Exception e)
         {
@@ -38,12 +46,12 @@ public class SpaceBattle_Tests
     [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
     public void SpaceshipInPosition(double x, double y)
     {
-        _spaceShip.SetCurrentPosition(new double[] {x,y});
+        _spaceShip.Position.Position = new double[] {x, y};
     }
     [Given(@"скорость корабля определить невозможно")]
     public void CantDefineInstantSpeed()
     {
-        _spaceShip.SetInstantSpeed(new double[] {double.NaN, double.NaN});
+        _spaceShip.Engine.UniformMotionSpeed = new double[] {double.NaN, double.NaN};
     }
      [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
     public void SpaceShipMoveToPoint(double x, double y)
@@ -51,12 +59,12 @@ public class SpaceBattle_Tests
         double[] expectedCoord = new double[] {x, y};
         for (int i = 0; i < 2; i++)
         {
-            Assert.Equal(expectedCoord[i], _spaceShip.CurrentPosition[i], 6);
+            Assert.Equal(expectedCoord[i], _spaceShip.Position.Position[i], 6);
         }
     }
     [Given(@"изменить положение в пространстве космического корабля невозможно")]
     public void CantChangePosition()
     {
-        _spaceShip.InstantSpeed = new double[] {double.NaN, double.NaN};
+        _spaceShip.Engine.UniformMotionSpeed = new double[] {double.NaN, double.NaN};
     }
 }
