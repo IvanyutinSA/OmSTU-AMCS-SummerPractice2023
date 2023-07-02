3a8e09ee685343e79742f5e112c8b0fa9a1669a5
IvanyutinSA <ivanyutin2004@gmail.com>
2023-07-02 20:35:41 +0000

    added json-parser

---
+++ SpaceCadets/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Newtonsoft;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        JObject tableJSON;
+        using (StreamReader reader = File.OpenText(args[0]))
+        {
+            tableJSON = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+        }
+
+        using (StreamWriter file = File.CreateText(args[1]))
+        using (JsonTextWriter writer = new JsonTextWriter(file))
+        {
+            tableJSON.WriteTo(writer);
+        }
+        Dictinory<String, List<Order>> table = JsonConvert.DeserializeObject<Dictinory<string, List<Order>>>(tableJSON);
+        Console.WriteLine(table["taskName"]);
+        
+    }
+}
